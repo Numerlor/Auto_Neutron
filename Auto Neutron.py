b5dcb3d47c073e0138065e1edbf9f2d2a142c81c
Numerlor <miso1038@gmail.com>
2019-06-26 21:49:39 +0200

    add traceback logging

--- Auto Neutron.py
+++ Auto Neutron.py
@@ -1,11 +1,11 @@
-import sys
 import os
+import sys
+import traceback
 
 from PyQt5 import QtWidgets, QtCore, QtGui
 
-from temp_saves import write_image, write_templates
-
 import MainWindows
+from temp_saves import write_image, write_templates
 
 
 # https://stackoverflow.com/a/44352931
@@ -15,15 +15,30 @@ def resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
+class ExceptionHandler:
+    def __init__(self, output_file):
+        self.path = output_file
+
+    def handler(self, exctype, value, tb):
+        f = open(self.path, 'w')
+        exception_lines = traceback.format_exception(exctype, value, tb)
+        for line in exception_lines:
+            f.write(line)
+        f.flush()
+        sys.__excepthook__(exctype, value, tb)
+
+
 if __name__ == "__main__":
     # save ahk templates and file image to temp file
     write_templates(resource_path("ahk"))
     write_image(resource_path(""))
     QtCore.QCoreApplication.setApplicationName("Auto Neutron")
     QtCore.QCoreApplication.setOrganizationName("Numerlor")
+    path = QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppConfigLocation)
+    # save traceback to logfile if Exception is raised
+    sys.excepthook = ExceptionHandler(path + "/traceback.log").handler
     app = QtWidgets.QApplication(sys.argv)
     app.setWindowIcon(QtGui.QIcon(resource_path("image.png")))
-    path = QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppConfigLocation)
     # create org and app folders if not found
     if not os.path.isdir(path):
         if not os.path.isdir(path[:path.rfind("/")]):
