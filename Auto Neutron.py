be70d321387222fb7632bb33b473077d1d79cee5
Numerlor <25886452+Numerlor@users.noreply.github.com>
2019-09-03 17:49:28 +0200

    use pathlib because we're cool kids

--- Auto Neutron.py
+++ Auto Neutron.py
@@ -1,8 +1,8 @@
-import os
-import sys
-
 import ctypes
+import sys
 import traceback
+from pathlib import Path
+
 from PyQt5 import QtWidgets, QtCore, QtGui
 
 import hub
@@ -13,8 +13,8 @@ from appinfo import APP, ORG, APPID
 # https://stackoverflow.com/a/44352931
 def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
-    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
-    return os.path.join(base_path, relative_path)
+    base_path = getattr(sys, '_MEIPASS', Path(__file__).absolute().parent)
+    return str(base_path / relative_path)
 
 class ExceptionHandler:
 
@@ -45,19 +45,17 @@ class ExceptionHandler:
 if __name__ == "__main__":
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(APPID)
     app = QtWidgets.QApplication(sys.argv)
-    app.setWindowIcon(QtGui.QIcon(resource_path("icons/icons_library.ico")))
+    app.setWindowIcon(QtGui.QIcon(resource_path(Path("icons/icons_library.ico"))))
     app.setApplicationName(APP)
     app.setOrganizationName(ORG)
 
-    path = QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppConfigLocation)
+    path = Path(QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppConfigLocation))
     # save traceback to logfile if Exception is raised
-    sys.excepthook = ExceptionHandler(path + "/traceback.log").handler
+    sys.excepthook = ExceptionHandler(path / "traceback.log").handler
     # create org and app folders if not found
-    if not os.path.isdir(path):
-        if not os.path.isdir(path[:path.rfind("/")]):
-            os.mkdir(path[:path.rfind("/")])
-        os.mkdir(path)
-    settings = QtCore.QSettings(path + "/config.ini", QtCore.QSettings.IniFormat)
+    if not path.exists():
+        path.mkdir(parents=True)
+    settings = QtCore.QSettings(str(path / "config.ini"), QtCore.QSettings.IniFormat)
     ui = hub.Hub(settings)
     ui.startup()
     sys.exit(app.exec_())
