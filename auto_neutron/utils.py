0b1f3f710af0dfb697ecafdeef5e80b2d46130b3
Numerlor <25886452+Numerlor@users.noreply.github.com>
2020-09-02 00:58:40 +0200

    Add logging setup

--- auto_neutron/utils.py
+++ auto_neutron/utils.py
@@ -1,12 +1,23 @@
+import logging
 import os
 import sys
 import traceback
 from pathlib import Path
-from typing import List
+from types import MethodType
+from typing import List, Optional, Tuple
 
 from PyQt5 import QtCore
 
 from auto_neutron.constants import JPATH
+
+
+QT_LOG_LEVELS = {
+    0: logging.DEBUG,
+    4: logging.INFO,
+    1: logging.WARNING,
+    2: logging.ERROR,
+    3: logging.CRITICAL,
+}
 
 
 def get_journals(n: int) -> List[Path]:
@@ -45,3 +56,19 @@ class ExceptionHandler(QtCore.QObject):
         sys.__excepthook__(exctype, value, tb)
 
         self.traceback_sig.emit(exc)
+
+
+def init_qt_logging(logger: logging.Logger) -> None:
+    """Redirect QDebug calls to `logger`."""
+    def handler(level: int, _context: QtCore.QMessageLogContext, message: str) -> None:
+        original_find_caller = logger.findCaller
+
+        def patched_caller(self, stack_info: bool) -> Tuple[str, int, str, Optional[str]]:  # noqa ANN001
+            """Patch filename to be "Qt" for logs from this function."""
+            _, lno, func, sinfo = original_find_caller(stack_info)
+            return "<Qt>", lno, func, sinfo
+
+        logger.findCaller = MethodType(patched_caller, logger)
+        logger.log(QT_LOG_LEVELS[level], message)
+        logger.findCaller = original_find_caller
+    QtCore.qInstallMessageHandler(handler)
--- main.py
+++ main.py
@@ -15,7 +15,9 @@
 # along with Auto_Neutron.  If not, see <https://www.gnu.org/licenses/>.
 
 import ctypes
+import logging
 import sys
+from logging import handlers
 from pathlib import Path
 
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -23,7 +25,7 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from auto_neutron import hub
 from auto_neutron.constants import APP, APPID, ORG
 from auto_neutron.settings import set_settings
-from auto_neutron.utils import ExceptionHandler
+from auto_neutron.utils import ExceptionHandler, init_qt_logging
 
 
 # https://stackoverflow.com/a/44352931
@@ -40,11 +42,27 @@ app.setApplicationName(APP)
 app.setOrganizationName(ORG)
 
 path = Path(QtCore.QStandardPaths.writableLocation(QtCore.QStandardPaths.AppConfigLocation))
+# create org and app folders
+path.mkdir(parents=True, exist_ok=True)
+
+log_format = logging.Formatter("{asctime} | {module:>7} | {levelname} | {message}", datefmt="%H:%M:%S", style="{")
+file_handler = handlers.RotatingFileHandler(path / "log.log", maxBytes=2*1024*1024, backupCount=3, encoding="utf8")
+file_handler.setFormatter(log_format)
+
+root_logger = logging.getLogger()
+root_logger.addHandler(file_handler)
+if __debug__:
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setFormatter(log_format)
+    root_logger.addHandler(stream_handler)
+    root_logger.setLevel(logging.DEBUG)
+else:
+    root_logger.setLevel(logging.INFO)
+init_qt_logging(root_logger)
+
 # save traceback to logfile if Exception is raised
 ex_handler = ExceptionHandler(path / "traceback.log")
 sys.excepthook = ex_handler.handler
-# create org and app folders
-path.mkdir(parents=True, exist_ok=True)
 
 set_settings(QtCore.QSettings(str(path / "config.ini"), QtCore.QSettings.IniFormat))
 ui = hub.Hub(ex_handler)
