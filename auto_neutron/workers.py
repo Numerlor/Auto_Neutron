a653dc8974053cd1dc61cb6484fc1c2ded1f311e
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-10-03 00:09:49 +0200

    Add status file worker

--- auto_neutron/workers.py
+++ auto_neutron/workers.py
@@ -1,7 +1,9 @@
 # This file is part of Auto_Neutron.
 # Copyright (C) 2019  Numerlor
 
+import collections.abc
 import contextlib
+import json
 import logging
 import subprocess  # noqa S404
 from functools import partial
@@ -10,6 +12,7 @@ from PySide6 import QtCore
 
 # noinspection PyUnresolvedReferences
 from __feature__ import snake_case, true_property  # noqa F401
+from auto_neutron.constants import STATUS_PATH
 from auto_neutron.journal import Journal
 from auto_neutron.route_plots import RouteList
 
@@ -54,3 +57,43 @@ class GameWorker(QtCore.QObject):
         self._timer.stop()
         self._generator.close()
         self._stopped = True
+
+
+class StatusWorker(QtCore.QObject):
+    """Follow the status file and dispatch `status_signal` from its contents."""
+
+    status_signal = QtCore.Signal(dict)
+
+    def __init__(self):
+        super().__init__()
+        self._generator = self.read_status()
+        self._timer = QtCore.QTimer()
+        self._timer.interval = 250
+        self._timer.timeout.connect(partial(next, self._generator))
+        self._running = False
+
+    def start(self) -> None:
+        """Start the worker to follow the status file."""
+        if self._running:
+            raise RuntimeError("Worker already started")
+        self._running = True
+        self._generator = self.read_status()
+        self._timer.start()
+
+    def stop(self) -> None:
+        """Stop following the status file."""
+        self._timer.stop()
+        self._generator.close()
+        self._running = False
+
+    def read_status(self) -> collections.abc[None, None, None]:
+        """Emit status_signal with the status dict on every status file change."""
+        last_content = None
+        with open(STATUS_PATH, encoding="utf8") as file:
+            while True:
+                file.seek(0)
+                content = file.read()
+                if content and content != last_content:
+                    self.status_signal.emit(json.loads(content))
+                    last_content = content
+                yield
