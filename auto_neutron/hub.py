e136b539b3fe8085965a13cfcc945007f6f078b5
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-10-18 18:24:16 +0200

    Add light blue text highlight color

--- auto_neutron/hub.py
+++ auto_neutron/hub.py
@@ -142,4 +142,6 @@ def set_theme() -> None:
         )
     else:
         p = app.style().standardPalette()
+    p.set_color(QtGui.QPalette.Highlight, QtGui.QColor(255, 255, 255, 0))
+    p.set_color(QtGui.QPalette.HighlightedText, QtGui.QColor(0, 123, 255))
     app.set_palette(p)
