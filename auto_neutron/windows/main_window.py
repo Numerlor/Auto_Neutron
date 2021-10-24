189993cc2b5df48d9bbc8d735993bb77b0e4d792
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-10-24 15:13:22 +0200

    Connect context menu copy action

--- auto_neutron/windows/main_window.py
+++ auto_neutron/windows/main_window.py
@@ -3,6 +3,10 @@
 
 import typing as t
 
+from PySide6 import QtWidgets
+
+# noinspection PyUnresolvedReferences
+from __feature__ import snake_case, true_property  # noqa: F401
 from auto_neutron.utils.utils import partial_no_external
 
 from .gui.main_window import MainWindowGUI
@@ -16,9 +20,15 @@ class MainWindow(MainWindowGUI):
         self.change_action.triggered.connect(
             lambda: self.table.edit_item(self.table.current_item())
         )
+        self.copy_action.triggered.connect(self.copy_table_item_text)
         self.conn = self.table.itemChanged.connect(
             partial_no_external(self.table.resize_column_to_contents, 0)
         )
+
+    def copy_table_item_text(self) -> None:
+        """Copy the text of the selected table item into the clipboard."""
+        if (item := self.table.current_item()) is not None:
+            QtWidgets.QApplication.instance().clipboard().set_text(item.text())
 
     def mass_insert(self, data: list[list[t.Any]]) -> None:
         """
