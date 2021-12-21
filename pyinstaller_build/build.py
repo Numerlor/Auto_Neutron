9dec3c1144ba734559f9e638dbf3017b04789136
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-12-21 19:37:21 +0100

    invoke pyinstaller directly as a python module

--- pyinstaller_build/build.py
+++ pyinstaller_build/build.py
@@ -5,12 +5,13 @@ import sys
 from pathlib import Path
 
 import dotenv
+import PyInstaller.__main__
 
 import delete_dlls
 
 dotenv.load_dotenv()
 
-if len(sys.argv) < 2 or sys.argv[1] not in {"--debug", "-d"}:
+if sys.flags.optimize:
     spec_files = ["Auto_Neutron.spec", "Auto_Neutron_dir.spec"]
     debug = False
 else:
@@ -19,19 +20,13 @@ else:
 delete_dlls.main()
 
 for spec_file in spec_files:
-    run_args = [
-        sys.executable,
-        "-m",
-        "PyInstaller",
-        spec_file,
-        f"--upx-dir={os.environ['UPX_DIR']}",
-        "-y",
-    ]
-
-    if not debug:
-        run_args.insert(1, "-OO")
-
-    subprocess.run(run_args)  # noqa S603
+    PyInstaller.__main__.run(
+        [
+            spec_file,
+            f"--upx-dir={os.environ['UPX_DIR']}",
+            "-y",
+        ]
+    )
 
 if not debug:
     if Path("dist/Auto_Neutron").exists():
