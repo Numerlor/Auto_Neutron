fd4b670c8d8fb16b937b3f222adaadc326bfbd48
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-12-21 15:28:33 +0100

    Don't run with python optimize in debug build

--- pyinstaller_build/build.py
+++ pyinstaller_build/build.py
@@ -7,18 +7,24 @@ import dotenv
 import delete_dlls
 
 dotenv.load_dotenv()
+
 if len(sys.argv) < 2 or sys.argv[1] not in {"--debug", "-d"}:
     spec_file = "Auto_Neutron.spec"
+    debug = False
 else:
     spec_file = "Auto_Neutron_debug.spec"
+    debug = True
 delete_dlls.main()
-subprocess.run(  # noqa S603
-    [
-        sys.executable,
-        "-OO",
-        "-m",
-        "PyInstaller",
-        spec_file,
-        f"--upx-dir={os.environ['UPX_DIR']}",
-    ]
-)
+
+run_args = [
+    sys.executable,
+    "-m",
+    "PyInstaller",
+    spec_file,
+    f"--upx-dir={os.environ['UPX_DIR']}",
+]
+
+if not debug:
+    run_args.insert(1, "-OO")
+
+subprocess.run(run_args)  # noqa S603
