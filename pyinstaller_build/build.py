60a4c1f4b389a8bb10244e1150371769392bde3c
Numerlor <25886452+Numerlor@users.noreply.github.com>
2021-12-22 03:14:00 +0100

    adjust build script to be run as a task from project root

--- pyinstaller_build/build.py
+++ pyinstaller_build/build.py
@@ -12,10 +12,13 @@ import delete_dlls
 dotenv.load_dotenv()
 
 if sys.flags.optimize:
-    spec_files = ["Auto_Neutron.spec", "Auto_Neutron_dir.spec"]
+    spec_files = [
+        "pyinstaller_build/Auto_Neutron.spec",
+        "pyinstaller_build/Auto_Neutron_dir.spec",
+    ]
     debug = False
 else:
-    spec_files = ["Auto_Neutron_debug.spec"]
+    spec_files = ["pyinstaller_build/Auto_Neutron_debug.spec"]
     debug = True
 delete_dlls.main()
 
@@ -25,9 +28,15 @@ for spec_file in spec_files:
             spec_file,
             f"--upx-dir={os.environ['UPX_DIR']}",
             "-y",
+            "--workpath=pyinstaller_build/build",
+            "--distpath=pyinstaller_build/dist",
         ]
     )
 
 if not debug:
-    if Path("dist/Auto_Neutron").exists():
-        shutil.make_archive("dist/Auto_Neutron", "zip", "dist/Auto_Neutron")
+    if Path("pyinstaller_build/dist/Auto_Neutron").exists():
+        shutil.make_archive(
+            "pyinstaller_build/dist/Auto_Neutron",
+            "zip",
+            "pyinstaller_build/dist/Auto_Neutron",
+        )
