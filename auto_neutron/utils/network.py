8d5dba7495cc3c086b246154ae98b6caf8859fd9
Numerlor <Numerlor@gmail.com>
2022-01-10 02:10:04 +0100

    fix typehint

--- auto_neutron/utils/network.py
+++ auto_neutron/utils/network.py
@@ -25,7 +25,7 @@ log = logging.getLogger(__name__)
 class NetworkError(Exception):
     """Raised for Qt network errors."""
 
-    def __init__(self, qt_error: str, reply_error: t.Optional[None]):
+    def __init__(self, qt_error: str, reply_error: t.Optional[str]):
         self.error_message = qt_error
         self.reply_error = reply_error
         super().__init__(qt_error, reply_error)
