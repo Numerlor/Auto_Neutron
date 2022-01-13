0346673d8d144d0e02e67db479e5e44e7e9d5fa9
Numerlor <Numerlor@gmail.com>
2022-01-14 00:35:00 +0100

    expose an initialize attribute on Ship
    checking for is None on the individual attributes was an inconsistent interface

--- auto_neutron/windows/new_route_window.py
+++ auto_neutron/windows/new_route_window.py
@@ -222,7 +222,7 @@ class NewRouteWindow(NewRouteWindowGUI):
 
     def _recalculate_range(self, cargo_mass: int) -> None:
         """Recalculate jump range with the new cargo_mass."""
-        if self.game_state.ship.fsd is not None:  # Ship may not be available yet
+        if self.game_state.ship.initialized:  # Ship may not be available yet
             self.spansh_neutron_tab.range_spin.value = self.game_state.ship.jump_range(
                 cargo_mass=cargo_mass
             )
