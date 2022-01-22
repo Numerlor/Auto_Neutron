2f3d0ec38a98c5ff0278353fe33595d481c70f78
Numerlor <Numerlor@gmail.com>
2022-01-22 23:46:16 +0100

    Sign the generated executables
    For now we're only using a self signed certificate

--- pyinstaller_build/build.py
+++ pyinstaller_build/build.py
@@ -33,6 +33,30 @@ def sha256sum(file: Path) -> str:
     return hash_.hexdigest()
 
 
+def sign_file(file: Path) -> subprocess.CompletedProcess:
+    """
+    Sign the file at `file`.
+
+    The SIGN_TOOL_PATH, TIMESTAMPING_URL, and CERT_SUBJECT env vars must be set.
+    """
+    return subprocess.run(  # noqa S603
+        [
+            os.environ["SIGN_TOOL_PATH"],
+            "sign",
+            "/tr",
+            os.environ["TIMESTAMPING_URL"],
+            "/td",
+            "sha256",
+            "/fd",
+            "sha256",
+            "/a",
+            "/n",
+            os.environ["CERT_SUBJECT"],
+            str(file),
+        ]
+    )
+
+
 if sys.flags.optimize:
     spec_files = [
         BASE_PATH / "Auto_Neutron.spec",
@@ -63,16 +87,30 @@ for spec_file in spec_files:
         ]
     )
 
+directory_path = Path(f"{BASE_PATH}/dist/Auto_Neutron")
+exe_path = directory_path.with_name("Auto_Neutron.exe")
+
 if not debug:
-    archive_path = shutil.make_archive(
-        f"{BASE_PATH}/dist/Auto_Neutron",
-        "zip",
-        f"{BASE_PATH}/dist/Auto_Neutron",
-    )
-    Path(archive_path + ".signature.txt").write_text(
-        sha256sum(Path(archive_path)) + "\n"
+    if os.environ.get("SIGN"):
+        if sign_file(directory_path / "Auto_neutron.exe").returncode != 0:
+            print("Failed to sign", directory_path / "Auto_neutron.exe")  # noqa T001
+
+    archive_path = Path(
+        shutil.make_archive(
+            str(directory_path),
+            "zip",
+            directory_path,
+        )
     )
 
-Path(f"{BASE_PATH}/dist/Auto_Neutron.exe.signature.txt").write_text(
-    sha256sum(Path(f"{BASE_PATH}/dist/Auto_Neutron.exe")) + "\n"
+    Path(archive_path.with_name(archive_path.name + ".signature.txt")).write_text(
+        sha256sum(archive_path) + "\n"
+    )
+
+if os.environ.get("SIGN"):
+    if sign_file(exe_path).returncode != 0:
+        print("Failed to sign", exe_path)  # noqa T001
+
+Path(exe_path.with_name(exe_path.name + ".signature.txt")).write_text(
+    sha256sum(exe_path) + "\n"
 )
