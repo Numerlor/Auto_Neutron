4da3c0c3f3d9e11603339f72ac369a37ba8932a7
Numerlor <Numerlor@gmail.com>
2022-01-22 23:19:52 +0100

    write hashes of generated files

--- pyinstaller_build/build.py
+++ pyinstaller_build/build.py
@@ -1,6 +1,7 @@
 # This file uses the MIT license.
 # Copyright (C) 2021  Numerlor
 
+import hashlib
 import os
 import shutil
 import subprocess  # noqa S404
@@ -14,6 +15,20 @@ import PyInstaller.__main__
 import delete_dlls
 
 dotenv.load_dotenv()
+
+
+def sha256sum(file: Path) -> str:
+    """Get the SHA256 checksum of `file`."""
+    hash_ = hashlib.sha256()
+    buffer = memoryview(bytearray(128 * 1024))
+    with file.open("rb", buffering=0) as f:
+        while True:
+            num_read = f.readinto(buffer)
+            if num_read == 0:
+                break
+            hash_.update(buffer[:num_read])
+    return hash_.hexdigest()
+
 
 if sys.flags.optimize:
     spec_files = [
@@ -45,8 +60,15 @@ for spec_file in spec_files:
 
 if not debug:
     if Path("pyinstaller_build/dist/Auto_Neutron").exists():
-        shutil.make_archive(
+        archive_path = shutil.make_archive(
             "pyinstaller_build/dist/Auto_Neutron",
             "zip",
             "pyinstaller_build/dist/Auto_Neutron",
         )
+        Path(archive_path + ".signature.txt").write_text(
+            sha256sum(Path(archive_path)) + "\n"
+        )
+
+Path("pyinstaller_build/dist/Auto_Neutron.exe.signature.txt").write_text(
+    sha256sum(Path("pyinstaller_build/dist/Auto_Neutron.exe")) + "\n"
+)
