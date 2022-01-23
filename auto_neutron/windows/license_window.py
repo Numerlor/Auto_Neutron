1e6e8ff7a4e4cf1594479768488eb2d96704c7a2
Numerlor <Numerlor@gmail.com>
2022-01-23 16:20:03 +0100

    Fix which strings are translated

--- auto_neutron/windows/license_window.py
+++ auto_neutron/windows/license_window.py
@@ -43,10 +43,10 @@ class LicenseWindow(LicenseWindowGUI):
         self.text.set_text(
             _("This program uses PySide6:<br>")
             + _("PySide6 Copyright (C) 2015 The Qt Company Ltd.") + "<br><br>"
-            + _("This program uses Python and its associated software:<br>")
+            + _("This program uses Python and its associated software:") + "<br>"
             + sys.copyright.replace("\n", "<br>") + "<br><br>"
             + _("Python is licensed under the ") + self.make_hyperlink(_("PSF License Agreement"), python_license_file_url)
-            + _(", see ") + self.make_hyperlink(_("docs.python.org/license.html"), PYTHON_LICENSE_URL) + " for more details.<br><br>"
+            + _(", see ") + self.make_hyperlink("docs.python.org/license.html", PYTHON_LICENSE_URL) + _(" for more details.<br><br>")
             + _("Auto_Neutron Copyright (C) 2019 Numerlor") + "<br>"
             + _("Auto_Neutron comes with ABSOLUTELY NO WARRANTY).") + "<br>"
             + _("This is free software, and you are welcome to redistribute it under the conditions of the ")
