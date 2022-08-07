2d48593a830bd758f5cad4ad06079c886fd8006c
Numerlor <Numerlor@gmail.com>
2022-08-07 22:05:42 +0200

    move fsd to its own module, booster out of consts

--- auto_neutron/constants.py
+++ auto_neutron/constants.py
@@ -4,7 +4,6 @@
 from __future__ import annotations
 
 import os
-import typing
 from pathlib import Path
 from string import Template
 
@@ -19,73 +18,12 @@ APPID = f"{ORG}|{APP}|{VERSION}"
 SPANSH_API_URL = "https://spansh.co.uk/api"
 
 
-class FrameShiftDrive(typing.NamedTuple):
-    """Hold information about a frame shift drive module."""
-
-    _SIZE_CONSTANTS = {2: 2.0, 3: 2.15, 4: 2.3, 5: 2.45, 6: 2.6, 7: 2.75}
-    _CLASS_CONSTANTS = {1: 11, 2: 10, 3: 8, 4: 10, 5: 12}
-    size: int
-    class_: int
-    max_fuel_usage: float
-    optimal_mass: float
-
-    @property
-    def rating_const(self) -> int:
-        """Get the linear rating const for the FSD's class rating."""
-        return self._CLASS_CONSTANTS[self.class_]
-
-    @property
-    def size_const(self) -> float:
-        """Get the power constant for the FSD's size."""
-        return self._SIZE_CONSTANTS[self.size]
-
-
 def get_config_dir() -> Path:
     """Create the config directory constant after the app was initialzied."""
     return Path(
         QtCore.QStandardPaths.writable_location(QtCore.QStandardPaths.AppConfigLocation)
     )
 
-
-FSD_CONSTANTS = {
-    "int_hyperdrive_size2_class1": FrameShiftDrive(2, 1, 0.60, 48),
-    "int_hyperdrive_size2_class2": FrameShiftDrive(2, 2, 0.60, 54),
-    "int_hyperdrive_size2_class3": FrameShiftDrive(2, 3, 0.60, 60),
-    "int_hyperdrive_size2_class4": FrameShiftDrive(2, 4, 0.80, 75),
-    "int_hyperdrive_size2_class5": FrameShiftDrive(2, 5, 0.90, 90),
-    "int_hyperdrive_size3_class1": FrameShiftDrive(3, 1, 1.20, 80),
-    "int_hyperdrive_size3_class2": FrameShiftDrive(3, 2, 1.20, 90),
-    "int_hyperdrive_size3_class3": FrameShiftDrive(3, 3, 1.20, 100),
-    "int_hyperdrive_size3_class4": FrameShiftDrive(3, 4, 1.50, 125),
-    "int_hyperdrive_size3_class5": FrameShiftDrive(3, 5, 1.80, 150),
-    "int_hyperdrive_size4_class1": FrameShiftDrive(4, 1, 2.00, 280),
-    "int_hyperdrive_size4_class2": FrameShiftDrive(4, 2, 2.00, 315),
-    "int_hyperdrive_size4_class3": FrameShiftDrive(4, 3, 2.00, 350),
-    "int_hyperdrive_size4_class4": FrameShiftDrive(4, 4, 2.50, 438),
-    "int_hyperdrive_size4_class5": FrameShiftDrive(4, 5, 3.00, 525),
-    "int_hyperdrive_size5_class1": FrameShiftDrive(5, 1, 3.30, 560),
-    "int_hyperdrive_size5_class2": FrameShiftDrive(5, 2, 3.30, 630),
-    "int_hyperdrive_size5_class3": FrameShiftDrive(5, 3, 3.30, 700),
-    "int_hyperdrive_size5_class4": FrameShiftDrive(5, 4, 4.10, 875),
-    "int_hyperdrive_size5_class5": FrameShiftDrive(5, 5, 5.00, 1050),
-    "int_hyperdrive_size6_class1": FrameShiftDrive(6, 1, 5.30, 960),
-    "int_hyperdrive_size6_class2": FrameShiftDrive(6, 2, 5.30, 1080),
-    "int_hyperdrive_size6_class3": FrameShiftDrive(6, 3, 5.30, 1200),
-    "int_hyperdrive_size6_class4": FrameShiftDrive(6, 4, 6.60, 1500),
-    "int_hyperdrive_size6_class5": FrameShiftDrive(6, 5, 8.00, 1800),
-    "int_hyperdrive_size7_class1": FrameShiftDrive(7, 1, 8.50, 1440),
-    "int_hyperdrive_size7_class2": FrameShiftDrive(7, 2, 8.50, 1620),
-    "int_hyperdrive_size7_class3": FrameShiftDrive(7, 3, 8.50, 1800),
-    "int_hyperdrive_size7_class4": FrameShiftDrive(7, 4, 10.60, 2250),
-    "int_hyperdrive_size7_class5": FrameShiftDrive(7, 5, 12.80, 2700),
-}
-BOOSTER_CONSTANTS = {
-    "int_guardianfsdbooster_size1": 4.0,
-    "int_guardianfsdbooster_size2": 6.0,
-    "int_guardianfsdbooster_size3": 7.75,
-    "int_guardianfsdbooster_size4": 9.25,
-    "int_guardianfsdbooster_size5": 10.5,
-}
 
 JOURNAL_PATH = (
     Path(os.environ["userprofile"])
---
+++ auto_neutron/fsd.py
@@ -0,0 +1,120 @@
+# This file is part of Auto_Neutron.
+# Copyright (C) 2019  Numerlor
+
+from __future__ import annotations
+
+import copy
+import dataclasses
+
+import more_itertools
+
+_FSD_SIZE_CONSTANTS = {2: 2.0, 3: 2.15, 4: 2.3, 5: 2.45, 6: 2.6, 7: 2.75}
+_FSD_CLASS_CONSTANTS = {1: 11, 2: 10, 3: 8, 4: 10, 5: 12}
+_RATING_TO_CLASS = {"A": 5, "B": 4, "C": 3, "D": 2, "E": 1}
+
+_DEEP_CHARGE_MODIFIER = 1.10  # max fuel usage increased by 10%
+_MASS_MANAGER_MODIFIER = 1.04  # Adds 4% to optimal mass
+
+
+@dataclasses.dataclass(slots=True, frozen=True)
+class FrameShiftDrive:
+    """Hold information about a frame shift drive module."""
+
+    size: int
+    class_: int
+    max_fuel_usage: float
+    optimal_mass: float
+
+    @property
+    def rating_const(self) -> int:
+        """Get the linear rating const for the FSD's class rating."""
+        return _FSD_CLASS_CONSTANTS[self.class_]
+
+    @property
+    def size_const(self) -> float:
+        """Get the power constant for the FSD's size."""
+        return _FSD_SIZE_CONSTANTS[self.size]
+
+    @classmethod
+    def from_loadout_dict(cls, loadout: dict) -> FrameShiftDrive:
+        """Create a FrameShiftDrive instance for the FSD in `loadout`."""
+        fsd_dict = more_itertools.only(
+            module
+            for module in loadout["Modules"]
+            if module["Slot"] == "FrameShiftDrive"
+        )
+        fsd = _BASE_FSDS[fsd_dict["Item"]]
+
+        engineering = fsd_dict.get("Engineering")
+        if engineering is not None:
+            fsd = copy.copy(fsd)
+            for modifier in engineering["Modifiers"]:
+                if modifier["Label"] == "FSDOptimalMass":
+                    object.__setattr__(fsd, "optimal_mass", modifier["Value"])
+                elif modifier["Label"] == "MaxFuelPerJump":
+                    object.__setattr__(fsd, "max_fuel_usage", modifier["Value"])
+        return fsd
+
+    @classmethod
+    def from_coriolis_dict(cls, coriolis_dict: dict) -> FrameShiftDrive:
+        """Create a FrameShiftDrive instance for the FSD in `coriolis_dict`."""
+        fsd_dict = coriolis_dict["components"]["standard"]["frameShiftDrive"]
+        class_ = _RATING_TO_CLASS[fsd_dict["rating"]]
+        size = fsd_dict["class"]
+
+        fsd = _BASE_FSDS[f"int_hyperdrive_size{size}_class{class_}"]
+        if (mod_dict := fsd_dict.get("modifications")) is not None:
+            fsd = copy.copy(fsd)
+            if (opt_mass_mod := mod_dict.get("optmass")) is not None:
+                object.__setattr__(
+                    fsd, "optimal_mass", fsd.optimal_mass * (1 + opt_mass_mod / 10000)
+                )
+
+            if (experimental_dict := fsd_dict["blueprint"].get("special")) is not None:
+                if experimental_dict["edname"] == "special_fsd_heavy":
+                    object.__setattr__(
+                        fsd,
+                        "optimal_mass",
+                        fsd.optimal_mass * _MASS_MANAGER_MODIFIER,
+                    )
+                elif experimental_dict["edname"] == "special_fsd_fuelcapacity":
+                    object.__setattr__(
+                        fsd,
+                        "max_fuel_usage",
+                        fsd.max_fuel_usage * _DEEP_CHARGE_MODIFIER,
+                    )
+        return fsd
+
+
+_BASE_FSDS = {  # base unengineered FSDs
+    "int_hyperdrive_size2_class1": FrameShiftDrive(2, 1, 0.60, 48),
+    "int_hyperdrive_size2_class2": FrameShiftDrive(2, 2, 0.60, 54),
+    "int_hyperdrive_size2_class3": FrameShiftDrive(2, 3, 0.60, 60),
+    "int_hyperdrive_size2_class4": FrameShiftDrive(2, 4, 0.80, 75),
+    "int_hyperdrive_size2_class5": FrameShiftDrive(2, 5, 0.90, 90),
+    "int_hyperdrive_size3_class1": FrameShiftDrive(3, 1, 1.20, 80),
+    "int_hyperdrive_size3_class2": FrameShiftDrive(3, 2, 1.20, 90),
+    "int_hyperdrive_size3_class3": FrameShiftDrive(3, 3, 1.20, 100),
+    "int_hyperdrive_size3_class4": FrameShiftDrive(3, 4, 1.50, 125),
+    "int_hyperdrive_size3_class5": FrameShiftDrive(3, 5, 1.80, 150),
+    "int_hyperdrive_size4_class1": FrameShiftDrive(4, 1, 2.00, 280),
+    "int_hyperdrive_size4_class2": FrameShiftDrive(4, 2, 2.00, 315),
+    "int_hyperdrive_size4_class3": FrameShiftDrive(4, 3, 2.00, 350),
+    "int_hyperdrive_size4_class4": FrameShiftDrive(4, 4, 2.50, 438),
+    "int_hyperdrive_size4_class5": FrameShiftDrive(4, 5, 3.00, 525),
+    "int_hyperdrive_size5_class1": FrameShiftDrive(5, 1, 3.30, 560),
+    "int_hyperdrive_size5_class2": FrameShiftDrive(5, 2, 3.30, 630),
+    "int_hyperdrive_size5_class3": FrameShiftDrive(5, 3, 3.30, 700),
+    "int_hyperdrive_size5_class4": FrameShiftDrive(5, 4, 4.10, 875),
+    "int_hyperdrive_size5_class5": FrameShiftDrive(5, 5, 5.00, 1050),
+    "int_hyperdrive_size6_class1": FrameShiftDrive(6, 1, 5.30, 960),
+    "int_hyperdrive_size6_class2": FrameShiftDrive(6, 2, 5.30, 1080),
+    "int_hyperdrive_size6_class3": FrameShiftDrive(6, 3, 5.30, 1200),
+    "int_hyperdrive_size6_class4": FrameShiftDrive(6, 4, 6.60, 1500),
+    "int_hyperdrive_size6_class5": FrameShiftDrive(6, 5, 8.00, 1800),
+    "int_hyperdrive_size7_class1": FrameShiftDrive(7, 1, 8.50, 1440),
+    "int_hyperdrive_size7_class2": FrameShiftDrive(7, 2, 8.50, 1620),
+    "int_hyperdrive_size7_class3": FrameShiftDrive(7, 3, 8.50, 1800),
+    "int_hyperdrive_size7_class4": FrameShiftDrive(7, 4, 10.60, 2250),
+    "int_hyperdrive_size7_class5": FrameShiftDrive(7, 5, 12.80, 2700),
+}
