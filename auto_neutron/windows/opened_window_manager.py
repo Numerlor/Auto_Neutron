563547d2718edadf2bb90bd18049c2fa3f758d95
Numerlor <Numerlor@gmail.com>
2022-09-09 02:36:52 +0200

    use function directly for window manager
    the class was only necessary for a QObject event_filter,
    with the evnet filter no longer used a function can be used directly

--- auto_neutron/windows/opened_window_manager.py
+++ auto_neutron/windows/opened_window_manager.py
@@ -5,42 +5,33 @@ import collections.abc
 import typing as t
 from functools import partial
 
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtWidgets
 
 _WindowT = t.TypeVar("_WindowT", bound=QtWidgets.QWidget)
 
 
-class _OpenedWindowManager(QtCore.QObject):
-    def __init__(self):
-        super().__init__()
-        self._window_cache = dict[
-            tuple[type[QtWidgets.QWidget], collections.abc.Hashable], QtWidgets.QWidget
-        ]()
+def create_or_activate_window(
+    window_type: type[_WindowT],
+    namespace: collections.abc.Hashable,
+    *args: object,
+    _window_cache: dict[
+        tuple[type[QtWidgets.QWidget], collections.abc.Hashable], QtWidgets.QWidget
+    ] = {},  # noqa: B006 used as persistent cache.
+    **kwargs: object,
+) -> _WindowT | None:
+    """
+    Create window of `window_type`, or bring it to the top if a window in the namespace is already opened.
 
-    def create_or_activate_window(
-        self,
-        window_type: type[_WindowT],
-        namespace: collections.abc.Hashable,
-        *args: object,
-        **kwargs: object,
-    ) -> _WindowT | None:
-        """
-        Create window of `window_type`, or bring it to the top if a window in the namespace is already opened.
-
-        If an existing window is brought to the top, return None, otherwise return the new window.
-        """
-        try:
-            existing_window = self._window_cache[(window_type, namespace)]
-        except KeyError:
-            new_window = window_type(*args, **kwargs)
-            window_key = (window_type, namespace)
-            self._window_cache[window_key] = new_window
-            new_window.destroyed.connect(partial(self._window_cache.pop, window_key))
-            return new_window
-        else:
-            existing_window.activate_window()
-            existing_window.raise_()
-
-
-_opened_window_manager = _OpenedWindowManager()
-create_or_activate_window = _opened_window_manager.create_or_activate_window
+    If an existing window is brought to the top, return None, otherwise return the new window.
+    """
+    try:
+        existing_window = _window_cache[(window_type, namespace)]
+    except KeyError:
+        new_window = window_type(*args, **kwargs)
+        window_key = (window_type, namespace)
+        _window_cache[window_key] = new_window
+        new_window.destroyed.connect(partial(_window_cache.pop, window_key))
+        return new_window
+    else:
+        existing_window.activate_window()
+        existing_window.raise_()
